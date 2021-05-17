9950fcf535f45f2586553087170db24c65733977
Shubham Sharma <shubsha@microsoft.com>
2021-05-17 08:33:23 -0700

    for safekeeping

--- myIsvService/Controllers/IsvServiceController.cs
+++ myIsvService/Controllers/IsvServiceController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,14 +15,17 @@ namespace myIsvService.Controllers
     [Route("[controller]")]
     public class IsvServiceController : ControllerBase
     {
-        private string clientId = "5c6fef57-2614-4e2f-a0ff-08687c7f4c51";
-        private string tenantId = "c59d6ef9-7063-4f77-9089-99d59ac78f35";
+        private string clientId = "5002759b-3b93-4c7e-bdc1-48a4b2842404";
+        private string tenantId = "d8cbc5c5-e484-48ea-af80-fc4083a2a740";
+        private string userId = "cbb6d774-d245-4927-9a4f-eea22c3f7ff4";
         private static string clientSecret = Environment.GetEnvironmentVariable(nameof(clientSecret));
 
+        private GraphServiceClient graphClient;
         private readonly ILogger<IsvServiceController> _logger;
 
         public IsvServiceController(ILogger<IsvServiceController> logger)
         {
+            graphClient = GetGraphClient();
             _logger = logger;
         }
 
@@ -36,9 +41,8 @@ namespace myIsvService.Controllers
         public async Task<string> CreateConnectionAndSchema([FromRoute]string externalConnectionId)
         {
             _logger.LogInformation("starting creating connection and schema.");
-            GraphServiceClient graphClient = GetGraphClient();
-            await CreateConnection(graphClient, externalConnectionId);
-            await CreateSchema(graphClient, externalConnectionId);
+            await CreateConnection(externalConnectionId);
+            await CreateSchema(externalConnectionId);
             return "";
         }
 
@@ -47,9 +51,60 @@ namespace myIsvService.Controllers
         public async Task<string> CreateItem([FromRoute] string externalConnectionId)
         {
             _logger.LogInformation("Ingesting item");
-            GraphServiceClient graphClient = GetGraphClient();
-            await IngestItem(graphClient, externalConnectionId);
+            await IngestItem(externalConnectionId);
             return "";
+        }
+
+        [HttpGet]
+        [Route("GetAllConnections")]
+        public async Task<string> GetAllConnections()
+        {
+            _logger.LogInformation("Getting all connections");
+
+            var connections = await graphClient.External.Connections
+                                                .Request()
+                                                .GetAsync();
+            StringBuilder response = new StringBuilder();
+
+            foreach(Microsoft.Graph.ExternalConnectors.ExternalConnection connection in connections)
+            {
+                response.Append($"{{ ConnectionName : {connection.Id} ConnectionState : {connection.State} Schema : {connection.Schema?.Id} }}");
+                response.Append("\n");
+            }
+
+            return response.ToString();
+        }
+
+        [HttpGet]
+        [Route("GetConnection/{externalConnectionId}")]
+        public async Task<string> GetConnection([FromRoute] string externalConnectionId)
+        {
+            _logger.LogInformation($"Getting external connection for : {externalConnectionId}");
+
+            var connection = await graphClient.External.Connections[externalConnectionId]
+                                                .Request()
+                                                .GetAsync();
+            return $"{{ ConnectionName : {connection.Id} ConnectionState : {connection.State} Schema : {connection.Schema?.Id} }}";
+        }
+
+        [HttpGet]
+        [Route("GetSchema/{externalConnectionId}")]
+        public async Task<string> GetSchema([FromRoute] string externalConnectionId)
+        {
+            _logger.LogInformation($"Getting schema for : {externalConnectionId}");
+
+            var schema = await graphClient.External.Connections[externalConnectionId].Schema
+                                                .Request()
+                                                .GetAsync();
+            var response = new StringBuilder("Schema\n");
+            if(schema != null)
+            {
+                foreach(Microsoft.Graph.ExternalConnectors.Property property in schema.Properties)
+                {
+                    response.Append(property.Name + " : " + property.Type).Append("\n");
+                }
+            }
+            return response.ToString();
         }
 
         private GraphServiceClient GetGraphClient()
@@ -64,57 +119,135 @@ namespace myIsvService.Controllers
             return new GraphServiceClient(authProvider);
         }
 
-        private async Task CreateConnection(GraphServiceClient graphClient, string externalConnectionId)
+        private async Task CreateConnection(string externalConnectionId)
         {
             _logger.LogInformation($"Creating connection for : {externalConnectionId}");
 
             var externalConnection = new Microsoft.Graph.ExternalConnectors.ExternalConnection
             {
                 Id = externalConnectionId,
-                Name = "myIsvServiceId connection",
+                Name = externalConnectionId,
                 Description = "Connection to upload data",
                 ODataType = null
             };
 
-            var response = await graphClient.External.Connections
-            .Request()
-            .AddAsync(externalConnection);
-
-            _logger.LogInformation($"Created connection with ID : {response.Id}");
+            try
+            {
+                var response = await graphClient.External.Connections
+                                                .Request()
+                                                .AddAsync(externalConnection);
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+                throw ex;
+            }
+            
+            _logger.LogInformation($"Created connection with ID : {externalConnectionId}");
         }
 
-        private async Task CreateSchema(GraphServiceClient graphClient, string externalConnectionId)
+        private async Task CreateSchema(string externalConnectionId)
         {
             _logger.LogInformation($"Creating schema for : {externalConnectionId}");
             var schema = new Microsoft.Graph.ExternalConnectors.Schema
             {
                 BaseType = "microsoft.graph.externalItem",
+                ODataType = null,
                 Properties = new List<Microsoft.Graph.ExternalConnectors.Property>()
                 {
                     new Microsoft.Graph.ExternalConnectors.Property
                     {
-                        Name = "userName",
+                        Name = "Question",
                         Type = Microsoft.Graph.ExternalConnectors.PropertyType.String,
                         IsSearchable = true,
                         IsRetrievable = true,
-                        IsQueryable = true
+                        Labels = new List<Microsoft.Graph.ExternalConnectors.Label>()
+                        {
+                            Microsoft.Graph.ExternalConnectors.Label.Title
+                        },
+                        ODataType = null
                     },
                     new Microsoft.Graph.ExternalConnectors.Property
                     {
-                        Name = "userId",
+                        Name = "Answer",
+                        Type = Microsoft.Graph.ExternalConnectors.PropertyType.String,
+                        IsSearchable = true,
+                        IsRetrievable = true,
+                        ODataType = null
+                    },
+                    new Microsoft.Graph.ExternalConnectors.Property
+                    {
+                        Name = "userName",
                         Type = Microsoft.Graph.ExternalConnectors.PropertyType.String,
                         IsQueryable = true,
                         IsRetrievable = true,
-                        IsSearchable = false
+                        IsSearchable = true,
+                        ODataType = null
                     }
                 }
             };
 
-            var response = await graphClient.External.Connections[externalConnectionId].Schema
-            .Request()
-            .CreateAsync(schema);
+            Microsoft.Graph.ExternalConnectors.Schema response = null;
+            try
+            {
+                response = await graphClient.External.Connections[externalConnectionId].Schema
+                                                .Request()
+                                                .Header("Prefer", "respond-async")
+                                                .CreateAsync(schema);
+                _logger.LogInformation(response?.Id);
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+                throw ex;
+            }
+            
 
             _logger.LogInformation($"Sent request to create schema with ID.");
+        } 
+
+        private async Task IngestItem(string externalConnectionId)
+        {
+            _logger.LogInformation($"writing object for {externalConnectionId}");
+
+            List<Microsoft.Graph.ExternalConnectors.Acl> acls = GetAcls();
+            Microsoft.Graph.ExternalConnectors.Properties properties = new Microsoft.Graph.ExternalConnectors.Properties
+            {
+                AdditionalData = new Dictionary<string, object>()
+                {
+                    {"userTitle", "CEO"},
+                    {"userName", "ISV Name"},
+                    {"userId", "ISV ID"}
+                }
+            };
+
+            var content = new Microsoft.Graph.ExternalConnectors.ExternalItemContent
+            {
+                Value = "CEO",
+                Type = Microsoft.Graph.ExternalConnectors.ExternalItemContentType.Text,
+                ODataType = null
+            };
+
+            Microsoft.Graph.ExternalConnectors.ExternalItem externalItem = new Microsoft.Graph.ExternalConnectors.ExternalItem
+            {
+                Acl = acls,
+                Properties = properties,
+                Content = content
+            };
+
+            try
+            {
+                var response = await graphClient.External.Connections[externalConnectionId].Items["TSP228082938"]
+                                                .Request()
+                                                .PutAsync(externalItem);
+                _logger.LogInformation(response?.Id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+                throw ex;
+            }
+            
+
+            _logger.LogInformation($"Done writing object for {externalConnectionId}");
         }
 
         private List<Microsoft.Graph.ExternalConnectors.Acl> GetAcls()
@@ -127,35 +260,15 @@ namespace myIsvService.Controllers
                     Value = clientId,
                     AccessType = Microsoft.Graph.ExternalConnectors.AccessType.Grant,
                     IdentitySource = Microsoft.Graph.ExternalConnectors.IdentitySourceType.AzureActiveDirectory
-                }
-            };
-        }
-
-        private async Task IngestItem(GraphServiceClient graphClient, string externalConnectionId)
-        {
-            _logger.LogInformation($"writing object for {externalConnectionId}");
-
-            List<Microsoft.Graph.ExternalConnectors.Acl> acls = GetAcls();
-            Microsoft.Graph.ExternalConnectors.Properties properties = new Microsoft.Graph.ExternalConnectors.Properties
-            {
-                AdditionalData = new Dictionary<string, object>()
+                },
+                new Microsoft.Graph.ExternalConnectors.Acl
                 {
-                    {"userName", "isvUserName"},
-                    {"userId", "1"}
+                    Type = Microsoft.Graph.ExternalConnectors.AclType.User,
+                    Value = userId,
+                    AccessType = Microsoft.Graph.ExternalConnectors.AccessType.Grant,
+                    IdentitySource = Microsoft.Graph.ExternalConnectors.IdentitySourceType.AzureActiveDirectory
                 }
             };
-
-            Microsoft.Graph.ExternalConnectors.ExternalItem externalItem = new Microsoft.Graph.ExternalConnectors.ExternalItem
-            {
-                Acl = acls,
-                Properties = properties
-            };
-
-            var response = await graphClient.External.Connections[externalConnectionId].Items[Guid.NewGuid().ToString()]
-                        .Request()
-                        .PutAsync(externalItem);
-
-            _logger.LogInformation($"Done writing object for {externalConnectionId}");
         }
     }
 }
---
+++ myIsvService/Utilities/ISVServiceUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myIsvService.Utilities
+{
+    public class ISVServiceUtilities
+    {
+        private static IDictionary<string, string> answersByQuestions = GetAnswersByQuestions();
+        private static List<string> userNames = GetUserNames();
+
+        private static IDictionary<string, string> GetAnswersByQuestions()
+        {
+            IDictionary<string, string> answersByQuestions = new Dictionary<string, string>();
+            answersByQuestions.Add("What is the name of the book", "Expo low");
+            answersByQuestions.Add("Who is the publisher of the book", "Problem publishing");
+
+            return answersByQuestions;
+        }
+
+        private static List<string> GetUserNames()
+        {
+            List<string> userNames = new List<string>();
+            userNames.Add("John Doe");
+            userNames.Add("Jane Doe");
+            return userNames;
+        }
+    }
+}
